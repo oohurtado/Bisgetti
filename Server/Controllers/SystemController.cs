9c0e5c126a41b95fc2f6c5edf15506b10f7ae296
oscar hurtado <oscar.hurtado@contabilizate.com>
2024-05-19 08:01:55 -0600

    system datetime

---
+++ Server/Controllers/SystemController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Source.Logic.User;
+using Server.Source.Models.DTOs.User.Settings.Admin;
+using System.Security.Claims;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemController : ControllerBase
+    {
+        [HttpGet(template: "datetime")]
+        public async Task<ActionResult> Datetime()
+        {
+            return Ok(await Task.FromResult(new { DateTime.Now }));
+        }  
+    }
+}
