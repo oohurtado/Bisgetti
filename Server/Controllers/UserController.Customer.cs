15b03b9a55003a7599d9ef489a3516472fac1e2a
oscar hurtado <oscar.hurtado@contabilizate.com>
2024-06-03 18:03:05 -0600

    TODO

---
+++ Server/Controllers/UserController.Customer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Source.Extensions;
+using Server.Source.Models.DTOs.User.Administration;
+using Server.Source.Models.DTOs.User.Common;
+using Server.Source.Models.Enums;
+using System.Security.Claims;
+
+namespace Server.Controllers
+{
+    public partial class UserController
+    {
+        // TODO: get, post, put, put - default, delete
+        // customer/address : GET
+        // customer/address : POST
+        // customer/address : PUT
+        // customer/address : DELETE
+        // customer/address : PUT - Default
+    }
+}
