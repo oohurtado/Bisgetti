fde805efc79f9837bbb9355225e1f9dbb70931e2
oscar hurtado <oscar.hurtado@contabilizate.com>
2024-06-05 08:39:23 -0600

    base my account

--- Client/src/app/components/my-account/base/base.component.ts
+++ Client/src/app/components/my-account/base/base.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from '../../../services/common/local-storage.service';
 
 @Component({
-  selector: 'app-base',
-  templateUrl: './base.component.html',
-  styleUrl: './base.component.css'
+    selector: 'app-base',
+    templateUrl: './base.component.html',
+    styleUrl: './base.component.css'
 })
 export class BaseComponent {
+
+    constructor(private localStorageService: LocalStorageService) {
+    }
+
+    isUserAdmin() {
+        return this.localStorageService.isUserAdmin();
+    }
+
+    isUserBoss() {
+        return this.localStorageService.isUserBoss();
+    }
+
+    isUserCustomer() {
+        return this.localStorageService.isUserCustomer();
+    }
 
 }
