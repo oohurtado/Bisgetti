30f2dd71c85f29f8ebe9bb4d59bb8131f9135ea6
oscar hurtado <oscar.hurtado@contabilizate.com>
2024-07-14 09:15:05 -0600

    todo

---
+++ Server/Controllers/BusinessController.MenuBuilder.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Source.Models.DTOs.Business.Category;
+
+namespace Server.Controllers
+{
+    public partial class BusinessController
+    {
+        // TODO: oohg menu builder
+
+        /*        
+            * Init
+                * obtener categorias
+                    * get:menu-builder/categories
+                    * GetCategories
+                * obtener productos
+                    * get:menu-builder/products
+                    * GetProducts
+                * obtener menu
+                    * get:menu-builder/menus/{menuId}
+                    * GetMenu
+        
+             * Imagenes                 
+                * agregar imagen a menu/categoria/producto
+                    * post
+                    * menu-builder/image/add-remove
+                    * AddImage(elementId, typeElement=menu/category/product, file)
+                *  quitar imagen de menu/categoria/producto
+                    * delete
+                    * menu-builder/image
+                    * DeleteImage(elementId, typeElement=menu/category/product)
+
+            * Elementos agregar/quitar
+                * agregar 'categoria a menu'/'producto a categoria'
+                    * post
+                    * menu-builder/add-remove
+                    * AddElement(elementId, elementType=category/product, toElementId, toElementType=menu/category)
+                *  quitar 'categoria de menu'/'producto de menu'
+                    *  delete
+                    *  menu-buider/element
+                    *  DeleteElement(elementId, elementType=category/product, fromElementId, fromElementType=menu/category)
+        
+             * Visibilidad 
+                - Mostrar/Ocultar = isVisible            
+                - Disponible/No disponible = isAvailable
+                - Vendido = isSoldOut
+                * actualizar ...
+                    * put
+                    * menu-builder/visibility
+                    * UpdateVisibilityElement(elementId, elementType=menu/category/product, field=visible/available/sold-out, value=true/false)
+
+            * Mover elementos en el menu
+                * move up/down
+                    * put
+                    * menu-builder/position
+                    * UpdatePositionElement(elementId, direction=up/down)       
+        */
+    }
+}
