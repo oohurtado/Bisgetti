837b8a8482c18737afe2531bb616de28e0c38f5d
oscar hurtado <oscar.hurtado@contabilizate.com>
2024-08-10 13:42:04 -0600

    borrando imagenes al borrar menu

--- Server/Source/Logic/BusinessLogic.Menu.cs
+++ Server/Source/Logic/BusinessLogic.Menu.cs
@@ -9,6 +9,9 @@ using Server.Source.Models.DTOs.Business.Menu;
 using Server.Source.Models.DTOs.Common;
 using Server.Source.Models.Entities;
 using Server.Source.Models.Enums;
+using Server.Source.Services.Interfaces;
+using Server.Source.Utilities;
+using System.Linq.Expressions;
 
 namespace Server.Source.Logic
 {
@@ -16,13 +19,18 @@ namespace Server.Source.Logic
     {
         private readonly IBusinessRepository _businessRepository;
         private readonly IMapper _mapper;
+        private readonly IStorageFile _storageFile;
+
+        private const string CONTAINER_FILE = "menu-images";
 
         public BusinessLogicMenu(
             IBusinessRepository businessRepository,
+            IStorageFile storageFile,
             IMapper mapper
             )
         {
             _businessRepository = businessRepository;
+            _storageFile = storageFile;
             _mapper = mapper;
         }
 
@@ -99,6 +107,21 @@ namespace Server.Source.Logic
             {
                 throw new EatSomeNotFoundErrorException(EnumResponseError.MenuNotFound);
             }
+
+            Expression<Func<MenuStuffEntity, bool>> exp = p => p.MenuId == id;
+            var elementsToDelete = await _businessRepository
+                .GetMenuStuff(exp)
+                .ToListAsync();
+
+            // borramos imagenes
+            elementsToDelete
+                .Where(p => !string.IsNullOrEmpty(p.Image))
+                .Select(p => p.Image)
+                .ToList()
+                .ForEach(async p =>
+                {
+                        await FileUtility.DeleteAsync(_storageFile, p!, CONTAINER_FILE);
+                    });
 
             await _businessRepository.DeleteMenuAsync(menu!);
         }
