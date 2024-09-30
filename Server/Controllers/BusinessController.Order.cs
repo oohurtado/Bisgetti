5de5f356431c2f67f7ee7454c6842fa154398a14
oscar hurtado <oscar.hurtado@contabilizate.com>
2024-09-29 20:02:25 -0600

    ...

--- Server/Controllers/BusinessController.Order.cs
+++ Server/Controllers/BusinessController.Order.cs
@@ -40,46 +40,46 @@ namespace Server.Controllers
         }
 
 
-        /// <summary>
-        /// Cambiar a siguiente paso
-        /// </summary>
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        [Authorize(Roles = "user-boss")]
-        [HttpPut(template: "orders/{orderId}/next-step")]
-        public async Task<ActionResult> OrderNextStep(int orderId, [FromBody] OrderNextStepRequest request)
-        {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier!)!;
-            var userRole = User.FindFirstValue(ClaimTypes.Role!);
-            var result = await _businessLogicOrder.OrderNextStepAsync(userId, userRole!, orderId, request);
-            return Ok(result);
-        }
+        ///// <summary>
+        ///// Cambiar a siguiente paso
+        ///// </summary>
+        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        //[Authorize(Roles = "user-boss")]
+        //[HttpPut(template: "orders/{orderId}/next-step")]
+        //public async Task<ActionResult> OrderNextStep(int orderId, [FromBody] OrderNextStepRequest request)
+        //{
+        //    var userId = User.FindFirstValue(ClaimTypes.NameIdentifier!)!;
+        //    var userRole = User.FindFirstValue(ClaimTypes.Role!);
+        //    var result = await _businessLogicOrder.OrderNextStepAsync(userId, userRole!, orderId, request);
+        //    return Ok(result);
+        //}
 
-        /// <summary>
-        /// Cambiar a cancelado
-        /// </summary>
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        [Authorize(Roles = "user-boss")]
-        [HttpPut(template: "orders/{orderId}/canceled")]
-        public async Task<ActionResult> OrderCanceled(int orderId, [FromBody] OrderCanceledRequest request)
-        {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier!)!;
-            var userRole = User.FindFirstValue(ClaimTypes.Role!);
-            await _businessLogicOrder.OrderCanceledAsync(userId, userRole!, orderId, request);
-            return Ok();
-        }
+        ///// <summary>
+        ///// Cambiar a cancelado
+        ///// </summary>
+        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        //[Authorize(Roles = "user-boss")]
+        //[HttpPut(template: "orders/{orderId}/canceled")]
+        //public async Task<ActionResult> OrderCanceled(int orderId, [FromBody] OrderCanceledRequest request)
+        //{
+        //    var userId = User.FindFirstValue(ClaimTypes.NameIdentifier!)!;
+        //    var userRole = User.FindFirstValue(ClaimTypes.Role!);
+        //    await _businessLogicOrder.OrderCanceledAsync(userId, userRole!, orderId, request);
+        //    return Ok();
+        //}
 
-        /// <summary>
-        /// Cambiar a declinado
-        /// </summary>
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        [Authorize(Roles = "user-boss")]
-        [HttpPut(template: "orders/{orderId}/declined")]
-        public async Task<ActionResult> OrderDeclined(int orderId, [FromBody] OrderDeclinedRequest request)
-        {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier!)!;
-            var userRole = User.FindFirstValue(ClaimTypes.Role!);
-            await _businessLogicOrder.OrderDeclinedAsync(userId, userRole!, orderId, request);
-            return Ok();
-        }
+        ///// <summary>
+        ///// Cambiar a declinado
+        ///// </summary>
+        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        //[Authorize(Roles = "user-boss")]
+        //[HttpPut(template: "orders/{orderId}/declined")]
+        //public async Task<ActionResult> OrderDeclined(int orderId, [FromBody] OrderDeclinedRequest request)
+        //{
+        //    var userId = User.FindFirstValue(ClaimTypes.NameIdentifier!)!;
+        //    var userRole = User.FindFirstValue(ClaimTypes.Role!);
+        //    await _businessLogicOrder.OrderDeclinedAsync(userId, userRole!, orderId, request);
+        //    return Ok();
+        //}
     }
 }
